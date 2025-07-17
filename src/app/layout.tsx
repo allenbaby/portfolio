8b5291fc4556ea59bc90b9e94a769fcaa838e897
allenbaby <allen.baby10@gmail.com>
2025-07-16 19:30:31 -0700

    mobile changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,31 +36,36 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <div className="fixed top-0 left-0 w-full h-full bg-black/30 z-[-1]" />
 
           {/* Theme toggle */}
-          <div className="fixed top-4 right-4 z-50">
+          <div className="fixed top-1 right-1" style={{ zIndex: 9999 }}>
             <ThemeToggle />
           </div>
+
 
           {/* Sidebar */}
           <LeftSidebar />
 
-          {/* Scroll progress on the right */}
-          <ScrollProgress />
+          {/* Scroll progress on the right with background */}
+          <div className="fixed top-0 right-0 h-full w-10 z-50
+                          bg-transparent md:bg-white dark:md:bg-black
+                          border-l border-transparent md:border-gray-200 dark:md:border-gray-800
+                        ">
+            <ScrollProgress />
+          </div>
+
 
           {/* Main content area with responsive padding */}
           <SmoothScrollProvider>
-            <main
-              className="
-                pt-0 pb-16  /* padding bottom for mobile sidebar height */
-                md:pl-16  /* padding left for desktop sidebar width */
-                md:pb-0   /* remove bottom padding on desktop */
-                mr-8      /* right margin for scroll progress bar width */
-              "
-            >
+            <main className="
+                              pt-0 pb-16      /* padding bottom for mobile sidebar */
+                              md:pl-16        /* padding left for desktop sidebar */
+                              md:pb-0         /* remove bottom padding on desktop */
+                              mr-0 md:mr-8    /* no right margin on mobile, margin on desktop */
+                            ">
               {children}
             </main>
           </SmoothScrollProvider>
         </ThemeProvider>
       </body>
-    </html>
+    </html >
   )
 }
